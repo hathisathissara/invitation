56e1c445679a902d9657d83dfdc8f8e2041ce77b
Your Name <hatheesha6504@gmail.com>
2026-07-20 10:06:41 +0530

    chore: generate service and package manifest caches

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -13,18 +13,4 @@
       0 => 'Carbon\\Laravel\\ServiceProvider',
     ),
   ),
-  'nunomaduro/collision' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
-    ),
-  ),
-  'nunomaduro/termwind' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
-    ),
-  ),
 );
